3df5d17e19c4fd51ada28cbcb4a6befeb0e7e397
Vanessa Pinheiro <vanessa05pinheiro@gmail.com>
2022-11-17 01:01:47 -0300

    API Test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta.entities.Sales;
+import com.devsuperior.dsmeta.services.SaleServices;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+	@Autowired
+	public SaleServices service;
+	@GetMapping
+	public List<Sales> findSale(){
+	return service.findSale();
+}
+}
---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleServices.java
@@ -0,0 +1,17 @@
+package com.devsuperior.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.devsuperior.dsmeta.entities.Sales;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+@Service
+public class SaleServices {
+	@Autowired
+	private SaleRepository repository;
+
+	public List<Sales> findSale() {
+		return repository.findAll();
+	}
+}
